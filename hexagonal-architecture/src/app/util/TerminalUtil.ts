0acac96f8954d4104a0a1339dca8304ef4223ff9
Matheus Vieira <matvno@gmail.com>
2024-02-19 14:26:21 -0300

    polymorphism 2

--- hexagonal-architecture/src/app/util/TerminalUtil.ts
+++ hexagonal-architecture/src/app/util/TerminalUtil.ts
@@ -11,6 +11,10 @@ export default class TerminalUtil {
         terminal.clear()
     }
 
+    static exibirChaveValor(chave: string, valor: any) {
+        terminal.yellow(chave).green(valor).white('\n')
+    }
+
     static async menu(opcoes: string[]): Promise<[number, string]> {
         const resposta = await terminal.singleColumnMenu(opcoes).promise
         return [resposta.selectedIndex, resposta.selectedText]
