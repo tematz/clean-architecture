7514cfb16ded2a4ce060647b1a5476b17c43840d
Matheus Vieira <matvno@gmail.com>
2024-02-20 10:40:44 -0300

    feat: interface use cases

--- hexagonal-architecture/src/core/usuario/service/RegistrarUsuario.ts
+++ hexagonal-architecture/src/core/usuario/service/RegistrarUsuario.ts
@@ -1,1 +1,8 @@
-export default class RegistrarUsuario { }
+import CasoDeUso from "@/core/shared/CasoDeUso";
+import Usuario from "../model/Usuario";
+
+export default class RegistrarUsuario implements CasoDeUso<Usuario, void {
+    async executar(usuario: Usuario): Promise<void> {
+
+    }
+}
