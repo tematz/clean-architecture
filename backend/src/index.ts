57f0150da85aa216a53ea961689194efa1371ee2
Matheus Vieira <matvno@gmail.com>
2024-02-23 13:11:58 -0300

    setup express

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,2 +1,12 @@
 import dotenv from 'dotenv'
+import express from 'express'
+
 dotenv.config()
+
+const app = express()
+const porta = process.env.API_PORT ?? 4000
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.listen(porta, () => { console.log(`Servidor executando na porta ${porta}!`) })
