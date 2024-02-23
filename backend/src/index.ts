e570b36a7db3a77a58b1c1c85936353f9256ff8b
Matheus Vieira <matvno@gmail.com>
2024-02-23 18:03:53 -0300

    feat: route register user

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,9 @@
 import dotenv from 'dotenv'
 import express from 'express'
+import RepositorioUsuarioPg from './external/db/RepositorioUsuarioPg'
+import SenhaCripto from './external/auth/SenhaCripto'
+import RegistrarUsuario from './core/usuario/service/RegistrarUsuario'
+import RegistrarUsuarioController from './external/api/RegistrarUsuarioControler'
 
 dotenv.config()
 
@@ -9,4 +13,12 @@ const porta = process.env.API_PORT ?? 4000
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.listen(porta, () => { console.log(`Servidor executando na porta ${porta}!`) })
+app.listen(porta, () => { console.log(`❤️‍🔥 Servidor executando na porta ${porta}!`) })
+
+// Rotas abertas
+
+const repositorioUsuario = new RepositorioUsuarioPg()
+const provedorCripto = new SenhaCripto()
+const registrarUsuario = new RegistrarUsuario(repositorioUsuario, provedorCripto)
+
+new RegistrarUsuarioController(app, registrarUsuario)
