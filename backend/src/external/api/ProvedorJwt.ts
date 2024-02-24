aaf4529ba3060dff5084c20b79e365617e691bad
Matheus Vieira <matvno@gmail.com>
2024-02-24 16:47:36 -0300

    feat: authentication middleware

--- backend/src/external/api/ProvedorJwt.ts
+++ backend/src/external/api/ProvedorJwt.ts
@@ -6,4 +6,8 @@ export default class ProvedorJwt {
     gerar(dados: string | object): string {
         return jwt.sign(dados, this.segredo, { expiresIn: '1d' })
     }
+
+    obter(token: string): string | object {
+        return jwt.verify(token, this.segredo)
+    }
 }
---
+++ backend/src/external/api/UsuarioMiddleware.ts
@@ -0,0 +1,32 @@
+import { NextFunction, Request, Response } from "express";
+import ProvedorJwt from "./ProvedorJwt";
+import Usuario from "@/core/usuario/model/Usuario";
+import RepositorioUsuario from "@/core/usuario/service/RepositorioUsuario";
+
+export default function UsuarioMiddleware(repositorio: RepositorioUsuario) {
+    return async (req: Request, resp: Response, next: NextFunction) => {
+        const acessoNegado = () => resp.status(403).send('Token inválido')
+
+        const token = req.headers.authorization?.replace('Bearer ', '')
+
+        if (!token) {
+            acessoNegado()
+            return
+        }
+
+        const provedorJwt = new ProvedorJwt(process.env.JWT_SECRET!)
+
+        const usuarioToken = provedorJwt.obter(token) as Usuario
+
+        const usuario = repositorio.buscarPorEmail(usuarioToken.email)
+
+        if (!usuario) {
+            acessoNegado()
+            return
+        }
+
+        (req as any).usuario = usuario
+
+        next()
+    }
+}
