8bd410167dcbb7e44cbd733ff1130286291831c4
Matheus Vieira <matvno@gmail.com>
2024-02-23 21:28:41 -0300

    fix: encryption providers

--- backend/src/external/auth/InverterSenhaCripto.ts
+++ backend/src/external/auth/InverterSenhaCripto.ts
@@ -3,7 +3,12 @@ import ProvedorCriptografia from "../../core/usuario/service/ProvedorCriptografi
 // Na arquitetura hexagonal esta classe é um Adaptador!
 // O adaptador NÃO faz parte do core da sua aplicação
 export default class InverterSenhaCripto implements ProvedorCriptografia {
+
     criptografar(senha: string): string {
         return senha.split('').reverse().join('')
+    }
+
+    comparar(senha: string, senhaCriptografada: string): boolean {
+        return this.criptografar(senha) === senhaCriptografada
     }
 }
--- backend/src/external/auth/SenhaCripto.ts
+++ backend/src/external/auth/SenhaCripto.ts
@@ -2,8 +2,13 @@ import ProvedorCriptografia from "@/core/usuario/service/ProvedorCriptografia"
 import bcrypt from 'bcrypt'
 
 export default class SenhaCripto implements ProvedorCriptografia {
+
     criptografar(texto: string): string {
         const salt = bcrypt.genSaltSync(10)
         return bcrypt.hashSync(texto, salt)
+    }
+
+    comparar(senha: string, senhaCriptografada: string): boolean {
+        return bcrypt.compareSync(senha, senhaCriptografada)
     }
 }
